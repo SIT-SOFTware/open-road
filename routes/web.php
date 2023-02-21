68d95c238ea648ce4221a7122f88d7b27aedfd51
“NMeyer1134” <nathaniel.meyer@lethbridgecollege.ca>
2023-02-21 13:30:33 -0700

    created test controller and added a route for it

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\testController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [testController::class, 'index']);
 
 Route::get('/test', function () {
     return view('test');
