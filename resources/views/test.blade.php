42920b9e0bf292abe6401eade7b26b60539797e6
JoshPHickman <josh.hickman@lethbridgecollege.ca>
2023-03-03 12:25:52 -0700

    Created initial layout view of header, nav, footer, and created common css directory and file in public directory

---
+++ resources/views/test.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="text-2xl">Welcome!!</h1>
+<p class="text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quibusdam, sint ea quae hic fugiat deleniti exercitationem dolor? Culpa, inventore.</p>
+@endsection
