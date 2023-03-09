6b2541a811ce13dc6de4aed3bb7207f3c0874a97
“NMeyer8386” <nathaniel.meyer@lethbridgecollege.ca>
2023-03-09 13:01:36 -0700

    pointed to the right table and tried to cast dates to a format

--- app/Models/PRA_Class.php
+++ app/Models/PRA_Class.php
@@ -11,5 +11,12 @@ class PRA_Class extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $table = 'pra_classes';
+
     protected $guarded = [];
+
+    protected $casts = [
+    'CLASS_START' => 'datetime:Y-m-d',
+    'CLASS_END' => 'datetime:Y-m-d',
+    ];
 }
