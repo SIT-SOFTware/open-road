de68eb4d53eb55ec15cfc145e94d7daf0dac9d60
“NMeyer8386” <nathaniel.meyer@lethbridgecollege.ca>
2023-03-10 18:08:07 -0700

    added some comments and cleaned up a bit

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -29,59 +29,67 @@
             <!-- Clicking the course name displays that course on its own page -->
             <!-- If course is trashed, don't display the course separately -->
             <div class="row align-items-center">
+
+                <!-- Conditionally renders buttons and deleted tag as well as prevents the user 
+                from navigating to the show page if they are in the trash-->
                 <div class="col-2">
                     <a style="font-size: 2.0rem; col-4 pr-10"
-                    
                     @if(request()->routeIs('admin.courses.index'))
                         href="{{ route('admin.courses.show', $course) }}" 
                     @endif
                     >{{ $course->COURSE_NAME }}</a>
-                    </div>
 
-                    @if(request()->routeIs('admin.trashed.courses.*'))
-                    <span class="col-6 align-bottom">
-                            <strong>Deleted: </strong> {{ $course->deleted_at->diffForHumans() }}
-                    </span>
-
-                    <form action="{{ route('admin.trashed.courses.update', $course) }}" method="post" class="col-1">
-                        @method('put')
-                        @csrf
-                        <button class="btn btn-success text-white col-12"><i class="bi bi-recycle"></i></button>
-                    </form>
-                    <form action="{{ route('admin.trashed.courses.destroy', $course) }}" method="post" class="col-1">
-                        @method('delete')
-                        @csrf
-                        <button class="btn btn-danger text-white col-12"><i class="bi bi-trash3"></i></button>
-                    </form>
-                    @endif
-            </div>
-                <div class="row">
-                    <!-- Course ID Display -->
-                    <div class="col">
-                        <div class="font-weight-bold"><u>Course ID:</u></div>
-                        <div>{{ $course->COURSE_ID }}</div>
-                    </div>
-                    <!-- Course Docs Display (WIP) -->
-
-                    <div class="col">
-                        <div class="font-weight-bold"><u>Course Documents:</u></div>
-                        <div>{{ $course->COURSE_DOCS }}</div>
-                    </div>
-
-                    <!-- Max Seats Display -->
-
-                    <div class="col">
-                        <div class="font-weight-bold"><u>Course Max Seats:</u></div>
-                        <div>{{ $course->COURSE_MAX_SEATS }}</div>
-                    </div>
-
-                    <!-- Course Fee Display -->
-                    
-                    <div class="col">
-                        <div class="font-weight-bold"><u>Course Fee:</u></div>
-                        <div>{{ $course->COURSE_FEE }}</div>
-                    </div>
                 </div>
+
+                <!-- Only renders if the user is on the trashed page -->
+                @if(request()->routeIs('admin.trashed.courses.*'))
+                <!-- Deleted tag -->
+                <span class="col-6 align-bottom">
+                        <strong>Deleted: </strong> {{ $course->deleted_at->diffForHumans() }}
+                </span>
+
+                <!-- Restore and Delete buttons -->
+                <form action="{{ route('admin.trashed.courses.update', $course) }}" method="post" class="col-1">
+                    @method('put')
+                    @csrf
+                    <button class="btn btn-success text-white col-12"><i class="bi bi-recycle"></i></button>
+                </form>
+                <form action="{{ route('admin.trashed.courses.destroy', $course) }}" method="post" class="col-1">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger text-white col-12"><i class="bi bi-trash3"></i></button>
+                </form>
+                @endif
+
+            </div>
+
+            <div class="row">
+                <!-- Course ID Display -->
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course ID:</u></div>
+                    <div>{{ $course->COURSE_ID }}</div>
+                </div>
+                <!-- Course Docs Display (WIP) -->
+
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course Documents:</u></div>
+                    <div>{{ $course->COURSE_DOCS }}</div>
+                </div>
+
+                <!-- Max Seats Display -->
+
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course Max Seats:</u></div>
+                    <div>{{ $course->COURSE_MAX_SEATS }}</div>
+                </div>
+
+                <!-- Course Fee Display -->
+                
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course Fee:</u></div>
+                    <div>{{ $course->COURSE_FEE }}</div>
+                </div>
+            </div>
         </div>          
         @endforeach
     </div>
