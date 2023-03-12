8cf1e578513ef2a0473771f76a88a33f569f98f9
JoshPHickman <90436701+JoshPHickman@users.noreply.github.com>
2023-03-12 03:19:42 -0600

    Added edit course view
    Brought course index in line with site styles while finalizing new admin dashboard UX.

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -1,7 +1,87 @@
 <x-app-layout>
+
+        <x-slot name="content">
     
-    <x-slot name="content">
-        <div>
+            <div class="container my-4 text-white">
+                <div class="card bg-dark p-3  shadow-lg">
+                    <div class="card-body">
+                        <form action="{{ route('admin.courses.update', $course) }}" method="post">
+                            @method('put')
+                            @csrf
+                            
+                            <!--Page Heading -->
+                            <div class="row justify-content-center">
+                                <div class="col-10 my-3">
+                                    <h1 class="card-heading text-white text-center">Editing Course: &nbsp;{{ $course->COURSE_NAME }}</h1>
+                                </div>
+                            </div>
+
+                            <!-- Name Input -->
+                            <div class="row justify-content-center ">
+                                <div class="col-lg-4 col-8 mb-lg-2 mt-lg-4 mb-4">
+                                    <div class="input-group">
+                                        <label class="input-group-text">Name</label>
+                                        <input required class="form-control" type="text" value="{{ @Old('courseName', $course->COURSE_NAME) }}" name="classCode">
+                                    </div>
+                                </div>
+
+                            </div>
+                            
+                            <div class="row mt-lg-4 justify-content-center">
+
+                                <!-- Course ID Input -->
+                                <div class="col-lg-2 col-8 mb-lg-2 mb-4">
+                                    <div class="input-group">
+                                        <label class="input-group-text">Course ID</label>
+                                        <input required class="form-control" type="number" value="{{ @Old('courseID', $course->COURSE_ID) }}" name="classCode">
+                                    </div>
+                                </div>
+
+                            
+                                <!-- Max Seats Input -->
+                                <div class="col-lg-2 col-8 mb-lg-2 mb-4">
+                                    <div class="input-group">
+                                        <label class="input-group-text">Max Seats</label>
+                                        <input required class="form-control" type="number" value="{{ @Old('courseMax', $course->COURSE_MAX_SEATS) }}" name="courseID">
+                                    </div>
+                                </div>
+                                
+                                <!-- Course Price Input -->
+                                <div class="col-lg-2 col-8 mb-lg-2 mb-4">
+                                    <div class="input-group">
+                                        <label class="input-group-text">Course Fee</label>
+                                        <input required class="form-control" type="number" value="{{ @Old('courseFee', $course->COURSE_FEE) }}" name="startDate">
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            
+                            <!-- Course Document Upload -->
+                            <div class="row justify-content-center">
+                                <div class="col-lg-3 col-8 mb-lg-2 mb-4 text-center">
+                                    <label class="text-decoration-underline my-3 fs-5">Course Documents</label>
+                                    <input id="courseDocsInput" type="file" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row justify-content-lg-end justify-content-center my-lg-4 my-3">
+                                
+                                <!-- Save & Delete Buttons -->
+                                <div class="col-auto">
+                                    <button class="btn btn-danger text-white px-3 me-3 mt-4 fs-5" onclick="return confirm('Do you want to trash this course?')"><i class="bi bi-trash3"></i></button>
+                                    <a href="{{ route('admin.courses.update', $course) }}" class="btn btn-success px-2 me-3 mt-4 fs-5">Save Changes</a>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        {{-- <div>
             <!-- Form updates changed data in the DB -->
             <form action="{{ route('admin.courses.update', $course) }}" class="border rounded p-3 bg-grey text-black" method="post">
                 @method('put')
@@ -58,7 +138,8 @@
                 <!-- Submits form to save course -->
                 <button class="btn btn-primary">Save Course</button>
             </form>
-        </div>
+        </div> --}}
+
     </x-slot>
     
-    </x-app-layout>
+</x-app-layout>
--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -6,6 +6,10 @@
     </x-alert-success>  
 
     <div class="container text-white">
+        
+        <!-- Title -->
+        <h1 class="text-dark text-center mt-3">Available Courses</h2>
+        <hr class="border border-dark" />
 
         <!-- Checks to see if the user is looking at the list of non-trashed courses -->
         @if(request()->routeIs('admin.courses.index'))
@@ -25,7 +29,7 @@
         <!-- Prints every course stored in the DB -->
         @forelse ( $courses as $course )
             @if(!request()->routeIs('admin.trashed.courses.*'))
-                <a href="{{ route('admin.courses.show', $course) }}" class="text-customWhite text-decoration-none">
+                <a href="{{ route('admin.courses.edit', $course) }}" class="text-customWhite text-decoration-none">
             @endif
                 <div class="row justify-content-center mb-3">
                     <div class="col">
@@ -65,18 +69,17 @@
                                             <form action="{{ route('admin.trashed.courses.update', $course) }}" method="post" class="">
                                                 @method('put')
                                                 @csrf
-                                                <button class="btn btn-success text-white" onclick="return confirm('Do you want to restore this course?')"><i class="bi bi-recycle"></i></button>
+                                                <button class="btn btn-success text-white"><i class="bi bi-recycle"></i></button>
                                             </form>
                                         </div>
-
+                                        
                                         <div class="col-auto">
                                             <form action="{{ route('admin.trashed.courses.destroy', $course) }}" method="post" class="">
                                                 @method('delete')
                                                 @csrf
-                                                <button class="btn btn-danger text-white" onclick="return confirm('Are you sure? This action cannot be undone')"><i class="bi bi-trash3"></i></button>
+                                                <button class="btn btn-danger text-white"><i class="bi bi-trash3"></i></button>
                                             </form>
                                         </div>
-                                        
                                     </div>
 
                                 @endif
@@ -90,7 +93,99 @@
             
         @endforeach
     </div>
+    
+    {{-- <div>
+        **TODO Whover coded this originally pls review and delete commented code when you think changes are fine
+        <x-alert-success>
+            {{ session('success') }}
+        </x-alert-success>        
 
+        <!-- Checks to see if the user is looking at the list of non-trashed courses -->
+        @if(request()->routeIs('admin.courses.index'))
+            <!-- Add Course Button and conditional render-->
+            <a href="{{ route('admin.courses.create') }}" class="btn btn-black">Add Course</a>
+
+            <!-- Go to trashed courses -->
+            <a href="{{ route('admin.trashed.courses.index') }}" class="btn btn-danger">Trashed</a>
+        @endif
+
+        <!-- Checks to see if the user is looking at trashed courses -->
+        @if(request()->routeIs('admin.trashed.courses.*'))
+
+            <!-- Add back button to return from trashed page -->
+            <a href="{{ route('admin.courses.index') }}" class="btn btn-danger">Back to Courses</a>
+
+        @endif
+
+        <!-- Prints every course stored in the DB -->
+        @forelse ( $courses as $course )
+        <div class="border rounded p-3 bg-black text-white">
+            <!-- Clicking the course name displays that course on its own page -->
+            <!-- If course is trashed, don't display the course separately -->
+            <div class="row align-items-center">
+
+                <!-- Conditionally renders buttons and deleted tag as well as prevents the user 
+                from navigating to the show page if they are in the trash-->
+                <div class="col-2">
+                    <a style="font-size: 2.0rem; col-4 pr-10"
+                        @if(request()->routeIs('admin.courses.index'))
+                            href="{{ route('admin.courses.show', $course) }}" 
+                        @endif
+                    >{{ $course->COURSE_NAME }}</a>
+
+                </div>
+
+                <!-- Only renders if the user is on the trashed page -->
+                @if(request()->routeIs('admin.trashed.courses.*'))
+                <!-- Deleted tag -->
+                <span class="col-6 align-bottom">
+                        <strong>Deleted: </strong> {{ $course->deleted_at->diffForHumans() }}
+                </span>
+
+                <!-- Restore and Delete buttons -->
+                <form action="{{ route('admin.trashed.courses.update', $course) }}" method="post" class="col-1">
+                    @method('put')
+                    @csrf
+                    <button class="btn btn-success text-white col-12"><i class="bi bi-recycle"></i></button>
+                </form>
+                <form action="{{ route('admin.trashed.courses.destroy', $course) }}" method="post" class="col-1">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger text-white col-12"><i class="bi bi-trash3"></i></button>
+                </form>
+                @endif
+
+            </div>
+            <div class="row">
+                <!-- Course ID Display -->
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course ID:</u></div>
+                    <div>{{ $course->COURSE_ID }}</div>
+                </div>
+                <!-- Course Docs Display (WIP) -->
+
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course Documents:</u></div>
+                    <div>{{ $course->COURSE_DOCS }}</div>
+                </div>
+
+                <!-- Max Seats Display -->
+
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course Max Seats:</u></div>
+                    <div>{{ $course->COURSE_MAX_SEATS }}</div>
+                </div>
+
+                <!-- Course Fee Display -->
+                
+                <div class="col">
+                    <div class="font-weight-bold"><u>Course Fee:</u></div>
+                    <div>{{ $course->COURSE_FEE }}</div>
+                </div>
+            </div>
+        </div>          
+        @endforeach
+    </div> --}}
 </x-slot>
 
 </x-app-layout>
